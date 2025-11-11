6bd5178ec99e8150d62c8a5915910769c6d47d4e
MikolajEkiert <mikieki2004@gmail.com>
2025-11-11 21:13:09 +0100

    add products table

---
+++ ZaawansowaneBazyDanych/Student_374/Tables/ProduktyM.sql
@@ -0,0 +1,6 @@
+?CREATE TABLE [Student_374].[ProduktyM] (
+    [ProductID] INT            IDENTITY (1, 1) NOT NULL,
+    [Name]      NVARCHAR (100) NULL,
+    [Category]  NVARCHAR (100) NULL,
+    [ListPrice] MONEY          NULL
+);
